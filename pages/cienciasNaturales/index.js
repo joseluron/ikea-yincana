b3ec87c0ef068a33c8b14777a648f563d7784516
Jose Luis <joseluron@gmail.com>
2021-05-28 01:12:16 +0200

    First blueprint for subject question

--- pages/cienciasNaturales/index.js
+++ pages/cienciasNaturales/index.js
@@ -1,5 +1,33 @@
+import queImg from "../../assets/ciencias-naturales/QUE.png";
+import tiendaImg from "../../assets/ciencias-naturales/TIENDA.png";
+import abrioImg from "../../assets/ciencias-naturales/ABRIO.png";
+import enImg from "../../assets/ciencias-naturales/EN.png";
+import dosmilcatorceImg from "../../assets/ciencias-naturales/DOSMILCATORCE.png";
+
+import styles from "../../styles/CienciasNaturales.module.scss";
+
 const CienciasNaturales = () => {
-    return <h1>CienciasNaturales</h1>;
-}
+  const QUESTION_1 = [queImg, tiendaImg, abrioImg, enImg, dosmilcatorceImg];
+
+  return (
+    <div className={styles.cienciasNnaturalesContainer}>
+      <h1>CienciasNaturales</h1>
+
+      <div className={styles.question}>
+        <h2>
+          Ejercicio 1.1: A través de la primera letra de cada foto, se
+          construira una frase. Revela dicha frase para poder contestar de forma
+          correcta a la pregunta que nos hace
+        </h2>
+
+        <div className={styles.questionImages}>
+          {QUESTION_1.map((image) => {
+            return <img src={image} alt="Primera pregunta" />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default CienciasNaturales;
